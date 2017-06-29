a24815067ed612b251eefb5f2fa0a207671d2f10
maryfranks <maryfranks@gmail.com>
2017-06-29 16:47:04 -0400

    added rolls to array and sorted

---
+++ rolls_sorted.rb
@@ -0,0 +1,12 @@
+# rolling a die
+
+sorted_rolls = []
+10.times do |roll|
+  roll = Random.rand(6) + 1
+  sorted_rolls << roll
+end
+sorted_rolls.sort!
+sorted_rolls.each do |roll|
+  puts "The result of your roll is #{roll}"
+end
+p sorted_rolls
