13159ba429b4422381b3b20522bc5662d05038a5
maryfranks <maryfranks@gmail.com>
2017-06-29 16:46:15 -0400

    updated code intenting for roll_the_die_ten_times

--- roll_die_ten_times.rb
+++ roll_die_ten_times.rb
@@ -1,5 +1,5 @@
 # rolling a die
 10.times do |roll|
-roll = Random.rand(6)
-puts "The result of your roll is #{roll + 1}"
+  roll = Random.rand(6)
+  puts "The result of your roll is #{roll + 1}"
 end
