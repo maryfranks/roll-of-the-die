0c32f00dc15da42b6185ed1dacd23ddb0d10a966
maryfranks <maryfranks@gmail.com>
2017-06-29 17:07:35 -0400

    added Doubles! announcements

---
+++ doubles.rb
@@ -0,0 +1,10 @@
+first_die = Random.rand(6) + 1
+second_die = Random.rand(6) + 1
+if first_die == second_die
+  puts "Your rolled a #{first_die} and a #{second_die}"
+  puts "Doubles!"
+  puts "Your total is #{first_die + second_die}"
+else
+  puts "Your rolled a #{first_die} and a #{second_die}"
+  puts "Your total is #{first_die + second_die}"
+end
